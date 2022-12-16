6ce26412fc8343823b257386f79015fa2d68687b
guihnrique <guiihenriquee@live.com>
2022-12-16 17:13:01 -0300

    Desafio SOLID - RocketSeat

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -6,10 +6,15 @@ interface IRequest {
 }
 
 class TurnUserAdminUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
-  execute({ user_id }: IRequest): User {
-    // Complete aqui
+  execute({ user_id }: IRequest): User | void {
+    const userExists = this.usersRepository.findById(user_id);
+    if (!userExists) {
+      throw new Error("User doesn't exists.");
+    }
+    const user = this.usersRepository.turnAdmin(userExists);
+    return user;
   }
 }
 
